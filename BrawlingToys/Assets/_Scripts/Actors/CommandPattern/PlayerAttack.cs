c0584f87b9ae9b5dba2524fc3ca2875fff53f3b4
giuRP <giulianoprofeti@gmail.com>
2024-09-04 11:14:26 -0300

    Combate - Atualização do prefab Player

--- BrawlingToys/Assets/_Scripts/Actors/CommandPattern/PlayerAttack.cs
+++ BrawlingToys/Assets/_Scripts/Actors/CommandPattern/PlayerAttack.cs
@@ -1,41 +1,54 @@
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour {
+namespace BrawlingToys.Actors
+{
+    public class PlayerAttack : MonoBehaviour
+    {
 
-    private ICommand _currentShootingCommand;
-    private ICommand _meleeCommand;
-    PlayerInputs _playerInputs;
+        private ICommand _currentShootingCommand;
+        private ICommand _meleeCommand;
+        PlayerInputs _playerInputs;
 
-    [Header(" - Shooting Cooldown - ")]
-    [SerializeField] private float _cooldown = 1.5f;
-    [SerializeField] private float _timer = 0.0f;
+        [Header(" - Shooting Cooldown - ")]
+        [SerializeField] private float _cooldown = 1.5f;
+        [SerializeField] private float _timer = 0.0f;
 
-    private void Awake() {
-        _playerInputs = GetComponent<PlayerInputs>();
-    }
-
-    private void Start() {
-        _timer = _cooldown;
-        _playerInputs.OnShootAction += PlayerInputs_OnAttackAction;
-    }
-
-    private void PlayerInputs_OnAttackAction(object sender, System.EventArgs e) {
-        if(_timer >= _cooldown) {
-            _currentShootingCommand?.Execute();
-            _timer = 0.0f;
-        } else {
-            _meleeCommand?.Execute();
+        private void Awake()
+        {
+            _playerInputs = GetComponent<PlayerInputs>();
         }
-    }
 
-    public void SetShootingCommand(ICommand command) {
-        _currentShootingCommand = command;
-    }
-    public void SetMeleeCommand(ICommand command) {
-        _meleeCommand = command;
-    }
+        private void Start()
+        {
+            _timer = _cooldown;
+            _playerInputs.OnShootAction += PlayerInputs_OnAttackAction;
+        }
 
-    private void Update() {
-        _timer += Time.deltaTime;
+        private void PlayerInputs_OnAttackAction(object sender, System.EventArgs e)
+        {
+            if (_timer >= _cooldown)
+            {
+                _currentShootingCommand?.Execute();
+                _timer = 0.0f;
+            }
+            else
+            {
+                _meleeCommand?.Execute();
+            }
+        }
+
+        public void SetShootingCommand(ICommand command)
+        {
+            _currentShootingCommand = command;
+        }
+        public void SetMeleeCommand(ICommand command)
+        {
+            _meleeCommand = command;
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+        }
     }
 }
