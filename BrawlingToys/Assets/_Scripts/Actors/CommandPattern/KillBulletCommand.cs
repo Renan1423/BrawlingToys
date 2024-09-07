62ad57b15ea972152f8072a7526e84e5a0d607e5
NicolasPegolo30 <nicolaspegolo@hotmail.com>
2024-09-07 16:13:52 -0300

    Multiplayer: Sistema de spawn no servidor
    - Nova classe "NetworkSpawner" criada para criar instancias no servidor.
    - Criando a network a prefab list para as balas que existem atualmente.
    - Criando um singleton para os network objects
    - Sincronizando as balas

--- BrawlingToys/Assets/_Scripts/Actors/CommandPattern/KillBulletCommand.cs
+++ BrawlingToys/Assets/_Scripts/Actors/CommandPattern/KillBulletCommand.cs
@@ -1,14 +1,23 @@
+using BrawlingToys.Network;
 using UnityEngine;
 
-public class KillBulletCommand : ICommand {
-    private Transform _firePoint;
+namespace BrawlingToys.Actors
+{
+    public class KillBulletCommand : ICommand
+    {
+        private Transform _firePoint;
 
-    public KillBulletCommand(Transform firePoint) {
-        _firePoint = firePoint;
-    }
+        public KillBulletCommand(Transform firePoint)
+        {
+            _firePoint = firePoint;
+        }
 
-    public void Execute() {
-        Debug.Log("KillerBullet");
-        GameObject bullet = Object.Instantiate(Resources.Load("KillerBullet"), _firePoint.position, _firePoint.rotation) as GameObject;
+        public void Execute()
+        {
+            Debug.Log("KillerBullet");
+            //GameObject bullet = Object.Instantiate(Resources.Load("KillerBullet"), _firePoint.position, _firePoint.rotation) as GameObject;
+            NetworkSpawner.LocalInstance.InstantiateOnServer("KillerBullet", _firePoint.position, _firePoint.rotation);
+        }
     }
 }
+
---
+++ BrawlingToys/Assets/_Scripts/Network/NetworkSpawner.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using BrawlingToys.Network;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace BrawlingToys.Network
+{
+    /// <summary>
+    /// This class manage the game object instantiation on server, this take care of all network responsibilities to do that on every client  
+    /// </summary>
+    public class NetworkSpawner : NetworkSingleton<NetworkSpawner> 
+    {
+        /// <summary>
+        /// Facilitates the search for network prefabs, associates the prefab with its name as a key
+        /// </summary>
+        private Dictionary<string, GameObject> _networkPrefabs = new(); 
+
+        /// <summary>
+        /// Called when any game object spawn on server, and return it
+        /// </summary>
+        public event Action<GameObject> WhenObjectSpawnedOnServer = delegate{ }; 
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            GenerateNetworkDictionary();
+        }
+
+        private void GenerateNetworkDictionary()
+        {
+            var networkPrefabLists = NetworkManager.Singleton.NetworkConfig.Prefabs.NetworkPrefabsLists;
+
+            foreach (var prefabList in networkPrefabLists)
+            {
+                foreach (var prefab in prefabList.PrefabList)
+                {
+                    _networkPrefabs.Add(prefab.Prefab.name, prefab.Prefab); 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Instantiate the object on server, for every connected client.
+        /// </summary>
+        /// <param name="gameObjectName">The name of the object to spawn.</param>
+        /// <param name="position">The position to spawn the object at.</param>
+        /// <param name="rotation">The rotation to spawn the object with.</param>
+        public void InstantiateOnServer(string gameObjectName, Vector3 position, Quaternion rotation)
+        {
+            if (!NameExistsInNetworkPrefabs()) MakeLogError(); 
+
+            SpawnServerRpc(gameObjectName, position, rotation); 
+
+            bool NameExistsInNetworkPrefabs() => _networkPrefabs.ContainsKey(gameObjectName); 
+            void MakeLogError() => 
+            Debug.LogError($"The object name {gameObjectName} dont match with network prefabs list names, please check network prefab names");
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void SpawnServerRpc(string gameObjectName, Vector3 position, Quaternion rotation)
+        {
+            var prefab = _networkPrefabs[gameObjectName]; 
+            
+            var objectToSpawn = Instantiate(prefab, position, rotation);
+            objectToSpawn.GetComponent<NetworkObject>().Spawn(true);
+
+            WhenObjectSpawnedOnServer.Invoke(objectToSpawn); 
+        }
+    }
+}
