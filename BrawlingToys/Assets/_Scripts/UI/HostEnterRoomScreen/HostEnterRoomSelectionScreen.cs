036bf4ea5e3e81cd17752e6a6f00c48069a192e8
Renan Nunes <rnnnunes97@gmail.com>
2024-10-17 15:43:47 -0300

    UI: Correção de detalhes no layout de criação de sala

---
+++ BrawlingToys/Assets/_Scripts/UI/HostEnterRoomScreen/HostEnterRoomSelectionScreen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrawlingToys.UI
+{
+    public class HostEnterRoomSelectionScreen : BaseScreen
+    {
+        
+    }
+}
