9969ec67d9a493dc12a778e13289bfd600440cef
giuRP <giulianoprofeti@gmail.com>
2024-10-20 11:53:02 -0300

    Combate : Classe PlayerWeapon implementada
    Funcionalidades de mirar e instanciar balas no server agora são da responsabilidade desta classe.

--- BrawlingToys/Assets/_Scripts/Actors/Player/Player.cs
+++ BrawlingToys/Assets/_Scripts/Actors/Player/Player.cs
@@ -17,7 +17,6 @@ namespace BrawlingToys.Actors
         /// A classe Player ser� o componente principal do objeto player(Pai) e funcionar� como um "container" dos principais atrubitos
         /// e componentes necess�rios para o funcionamento do objeto como um todo.
         /// </summary>
-        public event EventHandler<Vector3> OnUpdateAimRotation;
         public UnityEvent<Player> OnPlayerInitialize = new();
         public UnityEvent<Player> OnPlayerKill = new();
         public UnityEvent<Player> OnPlayerDeath = new();
@@ -34,6 +33,7 @@ namespace BrawlingToys.Actors
         public Rigidbody Rb { get => _rb; }
         public StateFactory StateFactory { get => _stateFactory; }
         public State CurrentState { get => _currentState; }
+        public PlayerWeapon Weapon { get => _weapon; }
 
         #endregion
 
@@ -54,8 +54,9 @@ namespace BrawlingToys.Actors
         [Header("Weapon Stuff: ")]
         [SerializeField] private LayerMask _groundLayerMask;
         [SerializeField] private Transform _firePoint; // Instancia a bala nesse game object
-        private RaycastHit hitInfo;
-        private float aimSmoothRate = 50f;
+        private RaycastHit _hitInfo;
+        private float _aimSmoothRate = 50f;
+        private PlayerWeapon _weapon;
 
         private StatsMediator _mediator;
 
@@ -92,7 +93,7 @@ namespace BrawlingToys.Actors
             _cooldowns.UpdateCooldowns();
 
             if (Application.isFocused && IsOwner)
-                HandleAim();
+                _weapon.Update();
         }
 
         private void FixedUpdate()
@@ -106,12 +107,14 @@ namespace BrawlingToys.Actors
         // e possivelmente modifica��es de buffs e debuffs.
         private void InitializePlayer()
         {
-            _baseStatsSO.defaultHitEffect = new KillCommand();
-            _mediator = new StatsMediator();
-            _stats = new Stats(_mediator, _baseStatsSO);
+            _baseStatsSO.defaultHitEffect = new();
+            _mediator = new();
+            _stats = new(_mediator, _baseStatsSO);
 
-            _cooldowns = new PlayerCooldownController(this);
+            _cooldowns = new(this);
             _cooldowns.Initialize();
+
+            _weapon = new(this, _firePoint, _aimSmoothRate, _groundLayerMask);
 
             OnPlayerInitialize?.Invoke(this);
         }
@@ -131,36 +134,10 @@ namespace BrawlingToys.Actors
             _currentState.Enter();
         }
 
-        private void HandleAim()
-        {
-            if (!_inputs.IsActive)
-                return;
-
-            Ray ray = Camera.main.ScreenPointToRay(_inputs.GetLookVector());
-            if (Physics.Raycast(ray, out hitInfo, float.MaxValue, _groundLayerMask))
-            {
-                transform.forward = Vector3.Slerp(transform.forward, hitInfo.point - transform.position, aimSmoothRate * Time.deltaTime);
-                OnUpdateAimRotation?.Invoke(this, transform.forward);
-            }
-        }
-
-        [ServerRpc(RequireOwnership = false)]
-        public void SpawnBulletServerRpc(string bulletName, ulong bulletOwnerPlayerId)
-        {
-            var bullet = NetworkSpawner
-                .LocalInstance
-                .InstantiateOnServer(bulletName, _firePoint.position, _firePoint.rotation)
-                .GetComponent<Bullet>();
-                
-            var owner = Instances.First(p => p.PlayerId == bulletOwnerPlayerId); 
-            
-            bullet.Initialize(owner); 
-        }
-
         private void OnDrawGizmos()
         {
             Gizmos.color = _aimColor;
-            Gizmos.DrawSphere(hitInfo.point, .5f);
+            Gizmos.DrawSphere(_hitInfo.point, .5f);
         }
     }
 }
