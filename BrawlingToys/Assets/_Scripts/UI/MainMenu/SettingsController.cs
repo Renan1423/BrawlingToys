90421d6fad7a63e647fac728cb670a18cf83934c
giuRP <giulianoprofeti@gmail.com>
2024-11-22 19:36:27 -0300

    MenuSettings : Salvamento das configurações implementada

--- BrawlingToys/Assets/_Scripts/UI/MainMenu/SettingsController.cs
+++ BrawlingToys/Assets/_Scripts/UI/MainMenu/SettingsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,53 +8,166 @@ namespace BrawlingToys.UI
 {
     public class SettingsController : MonoBehaviour
     {
-        [SerializeField] private TMP_Dropdown resolutionDropdown;
+        [Header("Toggles")]
+        [SerializeField] private Toggle _fullScreenToggle;
 
-        private Resolution[] resolutions;
+        [Space]
+        [Header("Dropdowns")]
+        [SerializeField] private TMP_Dropdown _resolutionDropdown;
+        [SerializeField] private TMP_Dropdown _qualityDropdown;
+
+        [Space]
+        [Header("Sliders")]
+        [SerializeField] private Slider _masterVolumeSlider;
+        [SerializeField] private Slider _musicVolumeSlider;
+        [SerializeField] private Slider _sfxVolumeSlider;
+        [SerializeField] private Slider _fpsSlider;
+
+        private Resolution[] _resolutions;
 
         private void Start()
         {
-            resolutions = Screen.resolutions;
+            _masterVolumeSlider.onValueChanged.AddListener(MasterVolumeSliderOnValueChange_Handler);
+            _musicVolumeSlider.onValueChanged.AddListener(MusicVolumeSliderOnValueChange_Handler);
+            _sfxVolumeSlider.onValueChanged.AddListener(SfxVolumeSliderOnValueChange_Handler);
 
-            resolutionDropdown.ClearOptions();
+            _resolutions = Screen.resolutions;
+
+            _resolutionDropdown.ClearOptions();
 
             List<string> resolutionOptions = new List<string>();
             int currentResolutionIndex = 0;
-            for (int i = 0; i < resolutions.Length; i++)
+            for (int i = 0; i < _resolutions.Length; i++)
             {
-                string option = resolutions[i].width + " x " + resolutions[i].height;
+                string option = _resolutions[i].width + " x " + _resolutions[i].height;
                 resolutionOptions.Add(option);
 
-                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
                     currentResolutionIndex = i;
             }
 
-            resolutionDropdown.AddOptions(resolutionOptions);
-            resolutionDropdown.value = currentResolutionIndex;
-            resolutionDropdown.RefreshShownValue();
+            _resolutionDropdown.AddOptions(resolutionOptions);
+
+            LoadSettings();
         }
 
-        // Volume controlado pelo Feel
+        private void OnDisable()
+        {
+            _masterVolumeSlider.onValueChanged.RemoveListener(MasterVolumeSliderOnValueChange_Handler);
+            _musicVolumeSlider.onValueChanged.RemoveListener(MusicVolumeSliderOnValueChange_Handler);
+            _sfxVolumeSlider.onValueChanged.RemoveListener(SfxVolumeSliderOnValueChange_Handler);
+        }
+
+        // Volume controlado pelo Feel. Aqui é apenas para salvar com PlayerPrefs
+        private void MasterVolumeSliderOnValueChange_Handler(float value)
+        {
+            PlayerPrefs.SetFloat("masterVolume", value);
+
+            _masterVolumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+        }
+
+        private void MusicVolumeSliderOnValueChange_Handler(float value)
+        {
+            PlayerPrefs.SetFloat("musicVolume", value);
+
+            _musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        }
+
+        private void SfxVolumeSliderOnValueChange_Handler(float value)
+        {
+            PlayerPrefs.SetFloat("sfxVolume", value);
+
+            _sfxVolumeSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        }
 
         public void SetFullScreen(bool value)
         {
             Screen.fullScreen = value;
+
+            if (value)
+                PlayerPrefs.SetInt("fullScreen", 1);
+            else
+                PlayerPrefs.SetInt("fullScreen", 0);
+
+            _fullScreenToggle.isOn = value;
         }
 
         public void SetResolution(int resolutionIndex)
         {
-            Resolution resolution = resolutions[resolutionIndex];
+            Resolution resolution = _resolutions[resolutionIndex];
             Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+            PlayerPrefs.SetInt("resolutionIndex", resolutionIndex);
+
+            _resolutionDropdown.value = PlayerPrefs.GetInt("resolutionIndex");
+            _resolutionDropdown.RefreshShownValue();
         }
 
         public void SetQuality(int qualityIndex)
         {
             QualitySettings.SetQualityLevel(qualityIndex);
+
+            PlayerPrefs.SetInt("qualityIndex", qualityIndex);
+
+            _qualityDropdown.value = PlayerPrefs.GetInt("qualityIndex");
+            _qualityDropdown.RefreshShownValue();
         }
 
-        public void SetTargetFPS(int targetFPS)
+        public void SetTargetFPS(float targetFPS)
         {
-            Application.targetFrameRate = targetFPS;
+            int int_targetFPS = Mathf.FloorToInt(targetFPS);
+
+            Application.targetFrameRate = int_targetFPS;
+
+            PlayerPrefs.SetInt("targetFPS", int_targetFPS);
+
+            _fpsSlider.value = PlayerPrefs.GetInt("targetFPS");
+        }
+
+        private void LoadSettings()
+        {
+            // Load Audio Settings
+            if (PlayerPrefs.HasKey("masterVolume"))
+                _masterVolumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+            else
+                _masterVolumeSlider.value = _masterVolumeSlider.maxValue;
+
+            if(PlayerPrefs.HasKey("musicVolume"))
+                _musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+            else
+                _musicVolumeSlider.value = _musicVolumeSlider.maxValue;
+
+            if (PlayerPrefs.HasKey("sfxVolume"))
+                _sfxVolumeSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+            else
+                _sfxVolumeSlider.value = _sfxVolumeSlider.maxValue;
+
+            // Load Screen Mode
+            if (PlayerPrefs.HasKey("fullScreen"))
+            {
+                bool value = PlayerPrefs.GetInt("fullScreen") > 0 ? true : false;
+                SetFullScreen(value);
+            }
+            else 
+                SetFullScreen(true);
+
+            // Load Resolution Settings
+            if (PlayerPrefs.HasKey("resolutionIndex"))
+                SetResolution(PlayerPrefs.GetInt("resolutionIndex"));
+            else
+                SetResolution(_resolutions.Length - 1);
+
+            // Load Quality Settings
+            if (PlayerPrefs.HasKey("qualityIndex"))
+                SetQuality(PlayerPrefs.GetInt("qualityIndex"));
+            else
+                SetQuality(QualitySettings.count - 1);
+
+            // Load FPS Settings
+            if (PlayerPrefs.HasKey("targetFPS"))
+                SetTargetFPS(PlayerPrefs.GetInt("targetFPS"));
+            else
+                SetTargetFPS(120);
         }
     }
 }
